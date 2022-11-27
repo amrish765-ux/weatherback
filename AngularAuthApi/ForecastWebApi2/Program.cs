0049fcb29b58ad10f912b01d105429f5f8bc383b
amrish <amrish01011999@gmail.com>
2022-11-27 21:28:28 +0530

    commit

---
+++ AngularAuthApi/ForecastWebApi2/Program.cs
@@ -0,0 +1,53 @@
+using ForecastWebApi2.Middlewares;
+
+namespace ForecastWebApi2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthorization();
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyPolicy",
+                    policy =>
+                    {
+                        policy.WithOrigins()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithMethods("PUT", "DELETE", "GET", "POST");
+                    });
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            
+            
+            app.UseMiddleware<CorsMiddleware>();
+            app.UseCors("MyPolicy");
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
